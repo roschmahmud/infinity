aa94ace7037f4c8a85934b6d70a2956444c4b4ef
roschmahmud <r.mahmud@outlook.de>
2017-08-24 13:59:46 +0200

    camera added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NativeStorage } from '@ionic-native/native-storage';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private memory: string;
+  private memories: any[];
 
+  private nativeStorage: NativeStorage;
+  
+  private camera: Camera;
+  private cameraOptions: CameraOptions;
+
+  constructor(navCtrl: NavController, nativeStorage: NativeStorage, camera: Camera) {
+    this.nativeStorage = nativeStorage;
+    
+    this.camera = camera;
+    this.cameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.ALLMEDIA
+    }
+
+    this.memories = [];
   }
+
+  enter() {
+    this.memories.push(this.memory);
+    this.memory = '';
+  }
+
+  openCamera() {
+    this.camera.getPicture(this.cameraOptions).then((data) => {
+      console.log('success!');
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  mic() {};
 
 }
